20ca885d885d4d27cbb9d5c8ee8ef37f8d2de1fb
Tony Mao <twm59@cornell.edu>
2023-03-14 23:07:31 -0400

    Added Initial Files
    - Main.ml takes in user input for number of players
    - Added dune files and OCaml forat files

---
+++ main.ml
@@ -0,0 +1,13 @@
+let rec main () =
+  print_endline
+    "Please enter the number of players for Texas Hold'em Poker, between 2 and \
+     10: \n";
+  print_endline ">";
+  let player_num = read_int () in
+  if player_num >= 2 && player_num <= 10 then
+    print_string ("You selected " ^ string_of_int player_num ^ " players.\n")
+  else (
+    print_string "Error: player number must be between 2 and 10.\n";
+    main ())
+
+let () = main ()
